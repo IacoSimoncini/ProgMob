338b1db9fd43d950f2054bf0c6b52307d7ec34fe
Iacopo <iaco.simoncini@gmail.com>
2020-06-29 19:50:31 +0200

    Aggiunto package Helpers, aggiunta classe AppData

---
+++ ProgMob/ProgMob.Android/Helpers/AppData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase;
+using Firebase.Database;
+
+namespace ProgMob.Droid.Helpers
+{
+    public static class AppData
+    {
+        public static FirebaseDatabase GetDatabase()
+        {
+            var app = FirebaseApp.InitializeApp(Application.Context);
+            FirebaseDatabase database;
+
+            if(app == null)
+            {
+                var option = new FirebaseOptions.Builder()
+                    .SetApplicationId("progmobxam-af105")
+                    .SetApiKey("AIzaSyDf7QAh_apHDta9HvV6ODs6jqLR-9nIlpw")
+                    .SetDatabaseUrl("https://progmobxam-af105.firebaseio.com")
+                    .SetStorageBucket("progmobxam-af105.appspot.com")
+                    .Build();
+
+                app = FirebaseApp.InitializeApp(Application.Context, option);
+                database = FirebaseDatabase.GetInstance(app);
+            }
+            else
+            {
+                database = FirebaseDatabase.GetInstance(app);
+            }
+
+            return database;
+        }
+    }
+}
