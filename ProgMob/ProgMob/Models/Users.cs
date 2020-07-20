505382ba39d155b1ec836a6988e764b170a67b19
RiccardoSchiavoni1998 <schiavonir1998@gmail.com>
2020-07-20 19:44:07 +0200

    fg

---
+++ ProgMob/ProgMob/Models/Users.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgMob.Models
+{
+    public class Users
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ ProgMob/ProgMob/Views/XF_SignUpPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProgMob.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class XF_SignUpPage : ContentPage
+    {
+        SignUpVM signUpVM;
+        public XF_SignUpPage()
+        {
+            InitializeComponent();
+            signUpVM = new SignUpVM();   
+            BindingContext = signUpVM;
+        }
+    }
+}
