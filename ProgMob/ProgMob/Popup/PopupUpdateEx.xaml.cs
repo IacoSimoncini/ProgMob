5ab2d3cdda37bf2c464179be7b6c2b5f1b8e48ef
Iacopo <iaco.simoncini@gmail.com>
2021-01-21 15:04:45 +0100

    Bug fix

--- ProgMob/ProgMob/Popup/PopupUpdateEx.xaml.cs
+++ ProgMob/ProgMob/Popup/PopupUpdateEx.xaml.cs
@@ -9,58 +9,49 @@ namespace ProgMob.Popup
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PopupUpdateEx : Rg.Plugins.Popup.Pages.PopupPage
     {
-        private readonly string exID;
-        private readonly string e_name;
-        private readonly string e_desc;
-        private readonly string e_diff;
-        public PopupUpdateEx(string id, string c_name , string c_desc , string c_diff)
+        private string exID;
+        private string e_name;
+        private string e_desc;
+        private string e_diff;
+        public PopupUpdateEx(string id, string c_name, string c_desc, string c_diff)
         {
-            InitializeComponent();
             e_name = c_name;
             e_desc = c_desc;
             e_diff = c_diff;
             exID = id;
+            InitializeComponent();
         }
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
-            string name = "";
-            string diff = "";
-            string desc = "";
-            
-            if (Name.Text != null){ name = Name.Text; }
-            else { name = e_name; };
-            if (Description.Text != null) { desc = Description.Text; }
-            else { desc = e_desc; };
-            if (Difficulty.SelectedItem.ToString() != null)
+            Console.WriteLine("1 " + e_name + " " + e_diff + " " + e_desc + " " + exID);
+            if (Name.Text != null) { e_name = Name.Text; }
+            if (Description.Text != null) { e_desc = Description.Text; }
+            if (Difficulty.SelectedItem != null)
             {
                 if (Difficulty.SelectedItem.ToString() == "Warming Up")
                 {
-                    diff = "Warming_Up.png";
+                    e_diff = "Warming_Up.png";
                 }
                 else if (Difficulty.SelectedItem.ToString() == "Arms")
                 {
-                    diff = "Arms.png";
+                    e_diff = "Arms.png";
                 }
                 else if (Difficulty.SelectedItem.ToString() == "Sit Ups")
                 {
-                    diff = "Sit_Ups.png";
+                    e_diff = "Sit_Ups.png";
                 }
                 else if (Difficulty.SelectedItem.ToString() == "Back")
                 {
-                    diff = "Back.png";
+                    e_diff = "Back.png";
                 }
                 else if (Difficulty.SelectedItem.ToString() == "Hit and Special")
                 {
-                    diff = "Hit_and_Special.png";
+                    e_diff = "Hit_and_Special.png";
                 }
             }
-            else
-            {
-                diff = e_diff;
-            }
-            Console.WriteLine("AAAAAAAAAAAAAAAAA " + name + " " + desc + " " + diff);
-            Exercise ex = new Exercise(name , desc , diff);
+            Console.WriteLine("2 " + e_name + " " + e_diff + " " + e_desc + " " + exID);
+            Exercise ex = new Exercise(e_name, e_desc, e_diff);
             ex.Id = exID;
             bool update = await DatabaseExercise.UpdateExercise(ex);
             if (update)
@@ -72,8 +63,7 @@ namespace ProgMob.Popup
             {
                 await App.Current.MainPage.DisplayAlert("Error", "Something went wrong", "OK");
                 PopupNavigation.PopAsync();
-                }
-            
+            }
         }
     }
 }
